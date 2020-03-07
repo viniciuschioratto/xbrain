ee0bb343391b720b96c12158fa48f33add8dc8f5
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2020-03-07 12:42:10 -0300

    Setup projeto

---
+++ src/main/java/com/xbrain/Xbrain/XbrainApplication.java
@@ -0,0 +1,13 @@
+package com.xbrain.Xbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xbrain/Xbrain/XbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.Xbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
